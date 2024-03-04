64c8da4bbeec5c4973bc6f2c82708fd5b1a5c7de
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-04 20:33:07 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -3,5 +3,5 @@ mod "spotify" {
   description   = "Navigate the soundscape of Spotify with the Spotify Dashboard, providing a deep dive into artist presence, album releases, track counts, and genre diversity, enriched with visual explorations of genre trends, artist popularity, and track popularity, using SQLite with Powerpipe."
   color         = "#1DB954"
   documentation = file("./README.md")
-  categories    = ["sqlite"]
+  categories    = ["dashboard", "sqlite"]
 }
