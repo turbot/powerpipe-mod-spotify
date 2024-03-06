d420b90b5632b09d257338ddbbc4e814a8f9c704
Ved misra <47312748+misraved@users.noreply.github.com>
2024-03-07 01:00:58 +0530

    Update mod.pp to use the correct path for mod documentation

--- mod.pp
+++ mod.pp
@@ -2,7 +2,7 @@ mod "spotify" {
   title         = "Spotify"
   description   = "Navigate the soundscape of Spotify with the Spotify Dashboard, providing a deep dive into artist presence, album releases, track counts, and genre diversity, enriched with visual explorations of genre trends, artist popularity, and track popularity, using SQLite with Powerpipe."
   color         = "#1DB954"
-  documentation = file("./index.md")
+  documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/spotify-insights.svg"
   categories    = ["dashboard", "sqlite"]
 
