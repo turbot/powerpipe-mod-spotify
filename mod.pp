b389c7f988b1de961459f5c0f4ef09e263412a21
sourav chakraborty <sourav@turbot.com>
2024-03-06 17:04:07 +0530

    update

--- mod.pp
+++ mod.pp
@@ -4,10 +4,10 @@ mod "spotify" {
   color         = "#1DB954"
   documentation = file("./README.md")
   categories    = ["dashboard", "sqlite"]
-}
 
-opengraph {
-  title       = "Powerpipe Mod for Spotify"
-  description = "Navigate the soundscape of Spotify with the Spotify Dashboard, providing a deep dive into artist presence, album releases, track counts, and genre diversity, enriched with visual explorations of genre trends, artist popularity, and track popularity, using SQLite with Powerpipe."
-  image       = "/images/mods/turbot/spotify-social-graphic.png"
+  opengraph {
+    title       = "Powerpipe Mod for Spotify"
+    description = "Navigate the soundscape of Spotify with the Spotify Dashboard, providing a deep dive into artist presence, album releases, track counts, and genre diversity, enriched with visual explorations of genre trends, artist popularity, and track popularity, using SQLite with Powerpipe."
+    image       = "/images/mods/turbot/spotify-social-graphic.png"
+  }
 }
